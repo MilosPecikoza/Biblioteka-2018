a5a10db4019e525b1c2678796cfbf1587b2334c0
MilosPecikoza <milospecikoza1997@gmail.com>
2018-03-03 16:52:17 +0100

    Inicijalni komit

---
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -0,0 +1,70 @@
+package biblioteka;
+
+import java.util.Arrays;
+
+public class Knjiga {
+	
+	private String naslov, isbn, izdavac;
+	private int izdanje;
+	private Autor[] autori;
+	
+	public String getNaslov() {
+		return naslov;
+	}
+	public void setNaslov(String naslov) {
+		this.naslov = naslov;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getIydavac() {
+		return izdavac;
+	}
+	public void setIydavac(String iydavac) {
+		this.izdavac = iydavac;
+	}
+	public int getIzdanje() {
+		return izdanje;
+	}
+	public void setIzdanje(int izdanje) {
+		this.izdanje = izdanje;
+	}
+	public Autor[] getAutori() {
+		return autori;
+	}
+	public void setAutori(Autor[] autori) {
+		this.autori = autori;
+	}
+	@Override
+	public String toString() {
+		return "Knjiga [naslov=" + naslov + ", isbn=" + isbn + ", iydavac=" + izdavac + ", izdanje=" + izdanje
+				+ ", autori=" + Arrays.toString(autori) + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((isbn == null) ? 0 : isbn.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Knjiga other = (Knjiga) obj;
+		if (isbn == null) {
+			if (other.isbn != null)
+				return false;
+		} else if (!isbn.equals(other.isbn))
+			return false;
+		return true;
+	}
+	
+}
